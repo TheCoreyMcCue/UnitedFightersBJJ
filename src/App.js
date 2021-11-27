d0d137ef2eb4331a6029a329c5793ec07e5f3715
thecoreymccue <corey.mccue@and.digital>
2021-11-27 19:54:00 +0100

    add some pages and navbar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
+import Classes from "./Components/Classes";
+import Contact from "./Components/Contact";
+import Navbar from "./Components/Navbar";
 
 import React from "react";
 
 const App = () => {
   return (
+    <div>
+      <Navbar />
+    <main>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route exact path="/" element={<Home />} />
       <Route exact path="/about" element={<About />} />
+      <Route exact path="/classes" element={<Classes />} />
+      <Route exact path="/contact" element={<Contact />} />
     </Routes>
+    </main>
+  </div>
   );
 };
 
