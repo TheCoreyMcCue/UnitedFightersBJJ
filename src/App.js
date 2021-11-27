a8be50d398e6ff10fc570f2b67adb8cf0e327110
thecoreymccue <corey.mccue@and.digital>
2021-11-27 18:22:17 +0100

    add react router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import "./App.css";
+// import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
 
 import React from "react";
 
 const App = () => {
-  return <div>hello werld</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route exact path="/about" element={<About />} />
+    </Routes>
+  );
 };
 
 export default App;
