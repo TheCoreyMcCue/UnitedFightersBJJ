e4a9a61d2fe73eb660d175fdc442113852d16ffa
thecoreymccue <corey.mccue@and.digital>
2021-11-28 12:10:30 +0100

    building architecture/favicon

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import About from "./Components/About";
 import Classes from "./Components/Classes";
 import Contact from "./Components/Contact";
 import Navbar from "./Components/Navbar";
+import Shop from "./Components/Shop";
 
 import React from "react";
 
@@ -17,6 +18,7 @@ const App = () => {
       <Route exact path="/" element={<Home />} />
       <Route exact path="/about" element={<About />} />
       <Route exact path="/classes" element={<Classes />} />
+      <Route exact path="/Shop" element={<Shop />} />
       <Route exact path="/contact" element={<Contact />} />
     </Routes>
     </main>
